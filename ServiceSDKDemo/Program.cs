d966ff59474ee4c1705b5d4435fddcff51c4e656
JanekPA <forxon54@gmail.com>
2025-01-06 12:21:32 +0100

    C2D, Direct Method, Device Twin - 1st lesson

---
+++ ServiceSDKDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Azure.Devices;
+using ServiceSDKDemo.Console;
+using ServiceSDKDemo.Library;
+
+string serviceConnectionString = "HostName=IoTProject2025.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=DBYODTeGXuh7hbFCZ56jS9nVTmWcIx8erAIoTAVjeT8=";
+
+using var serviceClient = ServiceClient.CreateFromConnectionString(serviceConnectionString);
+using var registryManager = RegistryManager.CreateFromConnectionString(serviceConnectionString);
+
+var manager = new IoTHubManager(serviceClient, registryManager);
+
+int input;
+do
+{
+    FeatureSelector.PrintMenu();
+    input = FeatureSelector.ReadInput();
+    await FeatureSelector.Execute(input, manager);
+} while (input != 0); 
